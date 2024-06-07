cfba5fb9c2fbed3d7bcb437d0f7b9c92fc87ab26
lanhx <lanhxdev0627@outlook.com>
2024-06-07 17:51:11 +0800

    feat

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,12 @@
 <template>
-  <div>
-    知能云-前端异常管理
-  </div>
+  <div>知能云-前端异常管理</div>
 </template>
 
 <script lang="ts" setup>
 onMounted(async () => {
+  const { data } = await useFetch("/api/track/catch");
+  console.log("data", data.value);
 });
 </script>
 
-<style>
-
-</style>
+<style></style>
--- server/api/track/catch.ts
+++ server/api/track/catch.ts
@@ -1,3 +1,13 @@
+import { serverSupabaseClient } from "#supabase/server";
 export default defineEventHandler(async (event) => {
-  return "Hello Nitro";
+  const supabase = await serverSupabaseClient(event);
+  const body = await readBody(event);
+  const { error } = await supabase
+    .from("requestError")
+    .insert({
+      request: JSON.stringify(body),
+      type: 1,
+      result: JSON.stringify(body),
+    } as any);
+  return { body };
 });
