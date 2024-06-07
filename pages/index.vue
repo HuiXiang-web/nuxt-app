d101f132eced4e7c308b28d060b4627e8788c654
lanhx <lanhxdev0627@outlook.com>
2024-06-07 16:16:30 +0800

    fix：测试跨域设置

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
+    知能云-前端异常管理
   </div>
 </template>
 
 <script lang="ts" setup>
 onMounted(async () => {
-  const { data } = await useFetch('/api/trackWeb')
-  console.log('data', data);
 });
 </script>
 
